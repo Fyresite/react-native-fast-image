15de98e2ed8f8e6107868b52a5b0c87c51f17977
Travis Delly <dellybro@gmail.com>
2020-01-22 15:24:19 -0700

    add constructors

--- android/src/main/java/com/dylanvann/fastimage/FastImageGlideUrl.java
+++ android/src/main/java/com/dylanvann/fastimage/FastImageGlideUrl.java
@@ -1,8 +1,26 @@
 package com.dylanvann.fastimage;
 
 import com.bumptech.glide.load.model.GlideUrl;
+import com.bumptech.glide.load.model.Headers;
+import java.net.URL;
 
 public class FastImageGlideUrl extends GlideUrl {
+    FastImageGlideUrl(URL url) {
+        super(url);
+    }
+
+    FastImageGlideUrl(String url) {
+        super(url);
+    }
+
+    FastImageGlideUrl(URL url, Headers headers) {
+        super(url, headers);
+    }
+
+    FastImageGlideUrl(String url, Headers headers) {
+        super(url, headers);
+    }
+
     @Override
     public String getCacheKey() {
         String url = toStringUrl();
